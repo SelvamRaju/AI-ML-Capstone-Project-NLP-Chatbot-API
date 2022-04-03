2ea9352ca3b3e667f7953ef4ce2be2378053a2c9
Selvam <officialselvam@gmail.com>
2022-04-03 11:47:10 +0530

    Update api.service.ts

--- src/app/service/api.service.ts
+++ src/app/service/api.service.ts
@@ -28,7 +28,7 @@ export class ApiService {
     return this.firebaseRef;
   }
 
-  getRandomInputValuesAPI(description: string) {
+  getRandomInputValues(description: string) {
     let valueSet = {"month":["January","February","March","April","May","June","July","August","September","October","November","December"],"country":["Country_01","Country_02","Country_03"],"local":["Local_01","Local_02","Local_03","Local_04","Local_05","Local_06","Local_07","Local_08","Local_09","Local_10","Local_11","Local_12"],"industrySector":["Mining","Metals","Others"],"genre":["Male","Female"],"employeeType":["Employee","Third Party","Third Party (Remote)"],"criticalRisk":["Pressed","Pressurized Systems","Manual Tools","Others","Fall prevention (same level)","Chemical substances","Liquid Metal","Electrical installation","Confined space","Pressurized Systems / Chemical Substances","Blocking and isolation of energies","Suspended Loads","Poll","Cut","Fall","Bees","Fall prevention","Not applicable","Traffic","Projection","Venomous Animals","Plates","Projection/Burning","remains of choco","Vehicles and Mobile Equipment","Projection/Choco","Machine Protection","Power lock","Burn","Projection/Manual Tools","Individual protection equipment","Electrical Shock","Projection of fragments"]};
     
     return {
